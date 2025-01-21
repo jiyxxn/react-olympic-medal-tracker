cf7a9db78c902533ddae3d65b10c10650796e07f
jinew <jiyunk@naver.com>
2025-01-21 20:30:05 +0900

    🎉 add : 컴포넌트 추가

---
+++ src/components/MedalListItems.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "./Button";
+
+const MedalListItems = ({ data }) => {
+  return (
+    <>
+      <tr>
+        <td>{data.nation}</td>
+        <td>{data.goldMedals}</td>
+        <td>{data.silverMedals}</td>
+        <td>{data.bronzeMedals}</td>
+        <td>
+          <Button text="삭제" type="button" />
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default MedalListItems;
