7fe8642e1a028c16bfe90614762d68fb0448cedd
jinew <jiyunk@naver.com>
2025-01-21 19:16:59 +0900

    ✨ Update : 입력 폼 UI 구현

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const InputField = ({ type, label, id, ...props }) => {
+const InputField = ({ type, label, id, onChange, value, ...props }) => {
   return (
     <label htmlFor={id}>
-      {label} <input type={type} id={id} />
+      {label}
+      <input type={type} id={id} onChange={onChange} value={value} />
     </label>
   );
 };
