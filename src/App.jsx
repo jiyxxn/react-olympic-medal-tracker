62326486f4bfed24e3463b77820f642b5479dbc9
jinew <jiyunk@naver.com>
2025-01-22 15:28:38 +0900

    ♻ Refactor : App.jsx에 필요한 state만 남기고 옮기기

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import MedalForm from "./components/MedalForm";
 import MedalList from "./components/MedalList";
-import { validatePositiveNumber } from "./utils/validateInput";
+import { updateLocalStorage } from "./utils/updateLocalStorage";
 
 function App() {
-  const [userInputs, setUserInputs] = useState({
-    nation: "",
-    goldMedals: "",
-    silverMedals: "",
-    bronzeMedals: "",
+  const storedCountries = JSON.parse(localStorage.getItem("medalList"));
+
+  const [medalList, setMedalList] = useState(() => {
+    return storedCountries ?? [];
   });
 
-  const [submittedData, setSubmittedData] = useState([]);
-
-  // * input 필드 초기화
-  const initializeUserInputs = () => {
-    setUserInputs({
-      nation: "",
-      goldMedals: "",
-      silverMedals: "",
-      bronzeMedals: "",
-    });
-  };
-
-  // * 사용자가 입력한 값을 state에 업데이트
-  const handleUserInputChange = (e) => {
-    const { id, value, type } = e.target;
-
-    if (type == "number" && !validatePositiveNumber(value)) {
-      alert("메달 개수는 0보다 작을 수 없습니다.");
-      return;
-    }
-
-    setUserInputs((prevState) => {
-      return { ...prevState, [id]: value };
-    });
-    console.log("userInputs =====>", userInputs);
-  };
-
-  // * 데이터 출력 및 입력 폼 초기화
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-
-    if (Object.values(userInputs).some((val) => val === "")) {
-      alert("작성되지 않은 값이 있습니다.");
-      return;
-    }
-
-    setSubmittedData((prevData) => {
-      if (prevData.some((data) => data.nation === userInputs.nation)) {
-        alert("이미 등록된 국가입니다. 업데이트를 이용해 주세요.");
-        return prevData;
-      } else {
-        return [...prevData, userInputs];
-      }
-    });
-
-    initializeUserInputs();
+  // * 로컬 스토리지에 저장
+  const saveMedalList = (country) => {
+    const newMedalList = [...medalList, country];
+    updateLocalStorage(setMedalList, "medalList", newMedalList);
   };
 
   return (
     <section>
       <h1>2024 파리 올림픽</h1>
-      <MedalForm
-        handleUserInputChange={handleUserInputChange}
-        handleFormSubmit={handleFormSubmit}
-        userInputs={userInputs}
-      />
+      <MedalForm saveMedalList={saveMedalList} />
 
-      {submittedData.length <= 0 && (
+      {medalList.length <= 0 && (
         <p className="default-message">
           아직 추가된 국가가 없습니다. 메달을 추적하세요!
         </p>
       )}
-      {submittedData.length > 0 && <MedalList userInputs={submittedData} />}
+      {medalList.length > 0 && <MedalList medalList={medalList} />}
     </section>
   );
 }
