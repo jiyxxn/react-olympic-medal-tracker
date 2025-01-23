120a2c13f8616f54fcdd84746d308e5dada6ff1b
jinew <jiyunk@naver.com>
2025-01-23 20:19:40 +0900

    🧹 Cleanup : 주석 추가

--- src/utils/updateLocalStorage.js
+++ src/utils/updateLocalStorage.js
@@ -1,3 +1,4 @@
+// 로컬 스토리지에 업데이트
 export const updateLocalStorage = (setList, storageKey, updatedList) => {
   setList(updatedList);
   localStorage.setItem(storageKey, JSON.stringify(updatedList));
